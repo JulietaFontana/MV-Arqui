e19ebd264351998558376333b34d49ec3b6b14f7
JulietaFontana <julietafontana9@gmail.com>
2026-09-19 18:43:11 -0300

    listo TDS e incializaciones

--- MV1.c
+++ MV1.c
@@ -3,11 +3,38 @@
 <include string.h>
 
 typedef struct{
+   short int base;
+   short int tamanio;
+}TablaSeg;
+
+typedef struct{
    unsigned char memoria[16384];
+   TablaSeg TDS[8];
+   unsigned int registros[32];
 }MV;
 
+
+void incializa(MV maquina, int tamCS){
+
+    maquina->TDS[0].base = 0;
+    maquina->TDS[0].tamanio = tamCS;
+    maquina->TDS[1].base = tamCS;
+    maquina->TDS[1].tamanio = 16384 - tamCS;
+    for (int i = 2; i < 8; i++) {
+        maquina->TDS[i].base = 0xFFFF;
+        maquina->TDS[i].tamanio = 0xFFFF;
+    }
+    //incializo resto de registros?
+    for (int i = 0; i < 32; i++) {
+    maquina->registros[i] = 0;
+    }
+    maquina->registros[26] = 0x00000000; // CS
+    maquina->registros[27] = 0x00010000; // DS
+    maquina->registros[0] = maquina->registros[26]; // IP
+}
+
 void leeArchivo(MV *maquina){
-    File *archb;
+    FILE *archb;
     char encabezado[8];
 
     archb = fopen("Prueba.vmx", "rb");
@@ -15,17 +42,19 @@ void leeArchivo(MV *maquina){
         printf("Error al abrir el archivo tipo .vmx\n");
     else{
         fread(encabezado,sizeof(char),8,archb);
-        if(strncmp(encabezado, "VMX26", 5) != 0 || encabezado[5]!=1){
-            printf("cabecera invalida");
+        if(strncmp(encabezado, "VMX26", 5) != 0 || encabezado[5]!=1){ //si tamCS es > 16384????? invalido?
+            printf("cabecera invalida"); 
             fclose(archb);
         }else{
             int tamCS =  ((unsigned char)encabezado[6] << 8) | (unsigned char)encabezado[7];
             fread(maquina->memoria, sizeof(char), tamCS, archb);
+            incializa(maquina, tamCS);
             fclose(archb);
         }
 
     }
 }
+
 
 
 void main(){
